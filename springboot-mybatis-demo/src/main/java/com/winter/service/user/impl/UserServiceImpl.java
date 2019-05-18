793458f58d6bd4663174a4722ae466bd3af37a99
huang405 <404405521@qq.com>
2019-05-18 19:07:39 +0800

    去掉1208

--- springboot-mybatis-demo/src/main/java/com/winter/service/user/impl/UserServiceImpl.java
+++ springboot-mybatis-demo/src/main/java/com/winter/service/user/impl/UserServiceImpl.java
@@ -27,7 +27,7 @@ public class UserServiceImpl implements UserService {
     /*
     * 这个方法中用到了我们开头配置依赖的分页插件pagehelper
     * 很简单，只需要在service层传入参数，然后将参数传递给一个插件的一个静态方法即可；
-    * pageNum 开始页数1208
+    * pageNum 开始页数
     * pageSize 每页显示的数据条数
     * create by hbt 1809
     * */

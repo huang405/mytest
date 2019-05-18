1b024de02e9a63dff633e0afbb15ebcae6dcd7d5
huang405 <404405521@qq.com>
2019-05-18 19:36:47 +0800

    加1

--- springboot-mybatis-demo/src/main/java/com/winter/service/user/impl/UserServiceImpl.java
+++ springboot-mybatis-demo/src/main/java/com/winter/service/user/impl/UserServiceImpl.java
@@ -33,6 +33,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAllUser(int pageNum, int pageSize) {
         //将参数传给这个方法就可以实现物理分页了，非常简单
+        //111
         PageHelper.startPage(pageNum, pageSize);
         return userMapper.selectAllUser();
     }
